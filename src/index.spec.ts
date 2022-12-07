ebe0c52b43e3748799db33b55470cbf02058a436
João Pedro Leôncio <joao.pedro@clarivate.com>
2022-12-07 16:15:11 +1300

    Updated tests

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -5,12 +5,23 @@ import { validateNHI } from './index'
 describe("validateNHI()", function () {
   describe("Valid", function () {
     const validExs = [
+      // old format
       "ZZZ0016",
       "ZZZ0024",
       "MCJ1984",
       "EPT6335",
       "CGC2720",
       "ZAC5361",
+      // new format
+      'ZBC42DQ',
+      'ZZZ00AX', 
+      'ZGT56KB', 
+      'ZHS91BR', 
+      'ZHW58CN', 
+      'ZLV86AX', 
+      'ZZZ00ZZ', 
+      'ZZZ99ZJ', 
+      'AAA00AD'
     ];
 
     validExs.forEach((ex) => {
@@ -27,6 +38,11 @@ describe("validateNHI()", function () {
       "ZZZ0044",
       "not an NHI",
       "1234567890",
+      // new format
+      'ZZZ00AA', 
+      'ZHW58CZ', 
+      'ZZZ99ZS', 
+      'AAA00AB'
     ];
 
     invalidExs.forEach((ex) => {
