62f38bb9157eaae851beccf166a393cee6db0578
Eli Lamb <eli@pixellab.nz>
2022-12-30 13:46:54 +1300

    Support for new format

--- src/validateNHI.ts
+++ src/validateNHI.ts
@@ -5,27 +5,35 @@
  * @returns `true` if the given string satisfies the New Zealand NHI Validation Routine and `false` otherwise
  *
  */
-export const validateNHI = (nhi: string) => {
-  // Steps 1 - 2
-  if (/^[A-HJ-NP-Z]{3}\d{4}$/.test(nhi)) {
-    // Character conversion numbers as defined by the NHI Validation Routine
-    const nhiConversionNums = [...nhi].map((c) =>
-      !isNaN(Number(c))
-        ? Number(c)
-        : c.charCodeAt(0) -
-          '@'.charCodeAt(0) -
-          (c > 'I' ? 1 : 0) -
-          (c > 'O' ? 1 : 0)
-    )
 
-    // Steps 3 - 10
-    const weightedVals = nhiConversionNums
-      .slice(0, -1)
-      .map((v, i) => v * (7 - i))
+const OLD_NHI_FORMAT_REGEX = new RegExp(/^[A-HJ-NP-Z]{3}\d{4}$/)
+const NEW_NHI_FORMAT_REGEX = new RegExp(/^[A-HJ-NP-Z]{3}\d{2}[A-HJ-NP-Z]{2}$/)
+
+export const validateNHI = (nhi: string) => {
+  nhi = nhi.toUpperCase()
+  const matchesOld = OLD_NHI_FORMAT_REGEX.test(nhi)
+  const matchesNew = NEW_NHI_FORMAT_REGEX.test(nhi)
+
+  // Character conversion numbers as defined by the NHI Validation Routine
+  const nhiConversionNums = [...nhi].map((c) =>
+    !isNaN(Number(c))
+      ? Number(c)
+      : c.charCodeAt(0) -
+        '@'.charCodeAt(0) -
+        (c > 'I' ? 1 : 0) -
+        (c > 'O' ? 1 : 0)
+  )
+
+  const weightedVals = nhiConversionNums.slice(0, -1).map((v, i) => v * (7 - i))
+
+  if (matchesNew) {
+    const checksum = weightedVals.reduce((acc, cur) => acc + cur, 0) % 24
+    const checkDigit = 24 - checksum
+    return checkDigit === nhiConversionNums[6]
+  }
+  if (matchesOld) {
     const checksum = weightedVals.reduce((acc, cur) => acc + cur, 0) % 11
-    // Steps 12 - 13
     const checkDigit = (11 - checksum) % 10
-    // Steps 11, 14
     return checksum !== 0 && checkDigit === nhiConversionNums[6]
   }
   return false
